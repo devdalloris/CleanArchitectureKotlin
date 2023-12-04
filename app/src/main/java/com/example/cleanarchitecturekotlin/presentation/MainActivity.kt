af5640b0180378eeb70c79d134a862305d062e06
dalloris <dalloris.uz@gmail.com>
2023-12-04 10:56:53 +0500

    Created storage

--- app/src/main/java/com/example/cleanarchitecturekotlin/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecturekotlin/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cleanarchitecturekotlin.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cleanarchitecturekotlin.data.repository.UserRepositoryImpl
+import com.example.cleanarchitecturekotlin.data.storage.sharedprefs.SharedPrefUserStorage
 import com.example.cleanarchitecturekotlin.databinding.ActivityMainBinding
 import com.example.cleanarchitecturekotlin.domain.models.SaveUserNameParam
 import com.example.cleanarchitecturekotlin.domain.usecase.GetUserNameUseCase
@@ -11,9 +12,23 @@ import com.example.cleanarchitecturekotlin.domain.usecase.SaveUserNameUseCase
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val userRepository by lazy(LazyThreadSafetyMode.NONE) { UserRepositoryImpl(context = applicationContext) }
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) { GetUserNameUseCase(userRepository = userRepository) }
-    private val saveUserNameParam by lazy(LazyThreadSafetyMode.NONE) { SaveUserNameUseCase(userRepository = userRepository) }
+    private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
+        UserRepositoryImpl(
+            userStorage = SharedPrefUserStorage(
+                context = applicationContext
+            )
+        )
+    }
+    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
+        GetUserNameUseCase(
+            userRepository = userRepository
+        )
+    }
+    private val saveUserNameParam by lazy(LazyThreadSafetyMode.NONE) {
+        SaveUserNameUseCase(
+            userRepository = userRepository
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
